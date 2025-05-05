5e384576518fcfd82dc39883b3f31e61633dcf91
Wholesome-Gee <jiyong0419@naver.com>
2025-05-05 14:05:32 +0900

    8.8

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { delay, motion, scale } from 'framer-motion'
-import { useRef } from "react";
+import { motion, useMotionValue } from "motion/react"
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -21,6 +21,13 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   background-color: white;
 `;
+const DragBox = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  background-color: white;
+`
 const BiggerBox = styled(motion.div)`
   width: 600px;
   height: 600px;
@@ -87,6 +94,11 @@ const dragVariant = {
 
 export default function App() {
   const biggerBox = useRef(null)
+  const x = useMotionValue(0)
+  // useMotionValue는 framer-motion 요소의 애니메이션 상태를 추적하고 제어한다. 허나, x값이 변한다해서 재렌더링이 일어나진 않음. 기본값은 0   #8.7
+  useEffect(()=>{
+    x.onChange(()=>console.log(x.get())) // motionValue x값을 받아오는 방법  #8.8
+  },[x])
   return (
     <Wrapper>
       {/* <motion.div></motion.div> */}
@@ -103,20 +115,24 @@ export default function App() {
         <Box drag dragSnapToOrigin dragElastic={0.5} dragConstraints={biggerBox} variants={dragVariant} whileDrag="drag"/>
         <Box drag dragConstraints={{top:-50, bottom:50, left:-50, right:50 }} variants={dragVariant} whileDrag="drag"/>
       </BiggerBox> */}
+      <DragBox style={{x}} drag="x" dragSnapToOrigin></DragBox>
+      <button onClick={()=>x.set(300)}>click</button>
     </Wrapper>
   );
 }
 
 /*
-92. div에 애니메이션 적용시킨다면 <motion.div> 라고 작성해야함  #8.1
-93. framer-motion으로 애니메이션 적용시키는법 : initial={{초기값}} animate={{변화값}} transition={{여러가지 애니메이션 옵션(문서확인)}}  #8.2
+104. div에 애니메이션 적용시킨다면 <motion.div> 라고 작성해야함  #8.1
+105. framer-motion으로 애니메이션 적용시키는법 : initial={{초기값}} animate={{변화값}} transition={{여러가지 애니메이션 옵션(문서확인)}}  #8.2
     - https://motion.dev/docs/react-transitions#duration
-94. framer-motion의 variants 기능을 활용하여 코드를 더욱 간단히 만들기  #8.3
-95~96. variants가 적용된 component는 자식 component에게도 자동으로 initial과 animate가 전해지게 된다. 고로 Circle에는 initial, animate가 생략 가능하다.  #8.4
-101. WhileHover, WhileTap 등 이벤트리스너를 활용해서 animation을 걸 수 있다.  #8.5
-102. BiggerBox를 dragConstraints로 사용하기 위해 ref를 지정  #8.6
-103. drag를 붙히면 drag가능한 요소가 되고, whileDrag를 통해서 animation을 걸 수 있다. (하지만 drag를 붙히는 순간 에러가 발생하여 강의 issue를 생성함)  #8.5
-103. dragSnapToOrigin은 드래그 종료 시 원위치 복귀, dragElastic={0~1}은 0일 수록 가운데에서 당기는 힘(중력)이 강해짐  #8.6
-104. dragConstraints(드래그 가능범위)를 직접 설정하기(비추)  #8.6
-105. dragConstraints={biggerBox}는 drag가능한 범위가 biggerBox 내부임을 나타낸다. biggerBox는 useRef로 인해 ReactJS로 접근할 수 있는 DOM요소가 된다.  #8.6
+106. framer-motion의 variants 기능을 활용하여 코드를 더욱 간단히 만들기  #8.3
+107~108. variants가 적용된 component는 자식 component에게도 자동으로 initial과 animate가 전해지게 된다. 고로 Circle에는 initial, animate가 생략 가능하다.  #8.4
+113. WhileHover, WhileTap 등 이벤트리스너를 활용해서 animation을 걸 수 있다.  #8.5
+114. BiggerBox를 dragConstraints로 사용하기 위해 ref를 지정  #8.6
+115. drag를 붙히면 drag가능한 요소가 되고, whileDrag를 통해서 animation을 걸 수 있다. (하지만 drag를 붙히는 순간 에러가 발생하여 강의 issue를 생성함)  #8.5
+     dragSnapToOrigin은 드래그 종료 시 원위치 복귀, dragElastic={0~1}은 0일 수록 가운데에서 당기는 힘(중력)이 강해짐  #8.6
+     dragConstraints={biggerBox}는 drag가능한 범위가 biggerBox 내부임을 나타낸다. biggerBox는 useRef로 인해 ReactJS로 접근할 수 있는 DOM요소가 된다.  #8.6
+116. dragConstraints(드래그 가능범위)를 직접 설정하기(비추)  #8.6
+118. useMotionValue인 x를 DragBox의 style에 적용함으로써 x는 DragBox의 animation 상태를 추적하고 컨트롤 할 수 있음.  #8.8
+119. x.set(300)으로 x(DragBox의 x값)을 300으로 설정하는 버튼
 */
